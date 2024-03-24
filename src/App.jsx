2513c05c8eed4729dbc3c65384b7a1c15123e18a
Ihar Baranau <baranovigorjsdev@gmail.com>
2024-03-24 15:03:45 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Heading from "./ui/Heading";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Row from "./ui/Row";
+
+const StyleApp = styled.main`
+  /* background-color: orangered; */
+  padding: 20px;
+`;
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <StyleApp>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
+
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button onClick={() => alert("check in")}>Check in</Button>
+              <Button
+                variations="secondary"
+                sizes="small"
+                onClick={() => alert("check out")}
+              >
+                Check out
+              </Button>
+            </div>
+          </Row>
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input placeholder="Number of guests"></Input>
+              <Input placeholder="Number of guests"></Input>
+            </form>
+          </Row>
+        </Row>
+      </StyleApp>
+    </>
+  );
+}
+
+export default App;
