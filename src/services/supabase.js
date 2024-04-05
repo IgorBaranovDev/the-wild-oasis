8ed1b36492df4b4a29198c725077d52aa835b714
Ihar Baranau <baranovigorjsdev@gmail.com>
2024-04-05 18:47:11 +0200

    feat: implemented upload image to supabase (#8)

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://wwjbpjltmwcvsmrnfexq.supabase.co";
+export const supabaseUrl = "https://wwjbpjltmwcvsmrnfexq.supabase.co";
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
