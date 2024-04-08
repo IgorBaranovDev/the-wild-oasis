0acbbdfa13c994fc51745a4a8b96f3a4ead5ad86
Ihar Baranau <baranovigorjsdev@gmail.com>
2024-04-08 15:04:37 +0200

    feat: refactor created custom hooks (#11)

--- src/features/cabins/CabinTable v1.jsx
+++ src/features/cabins/CabinTable v1.jsx
@@ -1,9 +1,10 @@
-import styled from "styled-components";
+import { useQuery } from "@tanstack/react-query";
+import { getCabins } from "../../services/apiCabins";
 
 import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
 
-import { useCabins } from "./useCabins";
+import styled from "styled-components";
 
 const Table = styled.div`
   border: 1px solid var(--color-grey-200);
@@ -30,7 +31,14 @@ const TableHeader = styled.header`
 `;
 
 function CabinTable() {
-  const { isLoading, cabins } = useCabins();
+  const {
+    isLoading,
+    data: cabins,
+    // error,
+  } = useQuery({
+    queryKey: ["cabin"],
+    queryFn: getCabins,
+  });
 
   if (isLoading) return <Spinner />;
 
---
+++ src/features/cabins/useCreateCabin.js
@@ -0,0 +1,25 @@
+import toast from "react-hot-toast";
+import { createEditCabin } from "../../services/apiCabins";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+export function useCreateCabin() {
+  const queryClient = useQueryClient();
+
+  const { mutate: createCabin, isLoading: isCreating } = useMutation({
+    // mutationFn: (newcabin) => createCabin(newcabin),
+    mutationFn: createEditCabin,
+    onSuccess: () => {
+      toast.success("New cabin successfully created");
+
+      queryClient.invalidateQueries({
+        queryKey: ["cabin"],
+      });
+    },
+    onError: (err) => toast.error(err.message),
+  });
+
+  return {
+    isCreating,
+    createCabin,
+  };
+}
