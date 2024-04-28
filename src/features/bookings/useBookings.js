24918c2d155920b62a9e6a00e8d691833688cf8f
Ihar Baranau <baranovigorjsdev@gmail.com>
2024-04-28 14:28:22 +0200

    feat:adding Optional Breakfast (#29)

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -22,6 +22,7 @@ export function useBookings() {
   // PAGINATION
   const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
 
+  // QUERY
   const {
     isLoading,
     data: { data: bookings, count } = {},
@@ -36,8 +37,14 @@ export function useBookings() {
 
   if (page < pageCount)
     queryClient.prefetchQuery({
-      queryKey: ["bookings", filter, sortBy, page],
+      queryKey: ["bookings", filter, sortBy, page + 1],
       queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
+    });
+
+  if (page > 1)
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page - 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page - 1 }),
     });
 
   return { isLoading, error, bookings, count };
--- src/ui/Pagination.jsx
+++ src/ui/Pagination.jsx
@@ -86,15 +86,16 @@ function Pagination({ count }) {
   return (
     <StyledPagination>
       <P>
-        Showing<span> {(currentPage - 1) * PAGE_SIZE + 1}</span> to{" "}
+        Showing <span>{(currentPage - 1) * PAGE_SIZE + 1}</span> to{" "}
         <span>
           {currentPage === pageCount ? count : currentPage * PAGE_SIZE}
-        </span>
+        </span>{" "}
         of <span>{count}</span> results
       </P>
+
       <Buttons>
         <PaginationButton onClick={prevPage} disabled={currentPage === 1}>
-          <HiChevronLeft /> <span>Previos</span>
+          <HiChevronLeft /> <span>Previous</span>
         </PaginationButton>
 
         <PaginationButton
