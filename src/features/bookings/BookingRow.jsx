b098f6f7224dda49daef782abdb34e777bcecd7b
Ihar Baranau <baranovigorjsdev@gmail.com>
2024-04-28 12:26:33 +0200

    feat:implemented chekin fn

--- src/features/bookings/BookingRow.jsx
+++ src/features/bookings/BookingRow.jsx
@@ -8,8 +8,9 @@ import { useNavigate } from "react-router-dom";
 
 import { formatCurrency } from "../../utils/helpers";
 import { formatDistanceFromNow } from "../../utils/helpers";
-import { HiEye } from "react-icons/hi";
+import { HiArrowDown, HiEye } from "react-icons/hi";
 import Menus from "../../ui/Menus";
+import { HiArrowDownOnSquare } from "react-icons/hi2";
 
 const Cabin = styled.div`
   font-size: 1.6rem;
@@ -96,6 +97,15 @@ function BookingRow({
           >
             See details
           </Menus.Button>
+
+          {status === "unconfirmed" && (
+            <Menus.Button
+              icon={<HiArrowDownOnSquare />}
+              onClick={() => navigate(`/checkin/${bookingId}`)}
+            >
+              Chek in
+            </Menus.Button>
+          )}
         </Menus.List>
       </Menus.Menu>
     </Table.Row>
