8655020c770c21de387bf6b64beea6253cd9f4e3
ibaranau1 <ibaranau1@exadel.com>
2024-06-19 20:59:42 +0200

    feta: create api and layout for dashboard

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import Spinner from "../../ui/Spinner";
+import { useRecentStays } from "./useRecentStays";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +9,23 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  const { bookings, isLoading: isLoading1 } = useRecentBookings();
+
+  const { isLoading: isLoading2, stays, confirmedStays } = useRecentStays();
+
+  if (isLoading2 || isLoading1) return <Spinner />;
+  console.log(bookings);
+
+  return (
+    <StyledDashboardLayout>
+      <div>Statistics</div>
+      <div>Today activity</div>
+      <div>Chart stay durations</div>
+      <div>Chart sales</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
